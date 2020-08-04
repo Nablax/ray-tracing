39158b500c1f8cc08703da3e87f11792fb8595bb
cosmos <kbxpy12321@gmail.com>
2020-08-04 15:34:24 -0500

    hit sph simplify

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
+    auto half_b = dot(oc, r.direction());
     auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
+    auto discriminant = half_b * half_b - a * c;
     if (discriminant < 0) {
         return -1.0;
     } else {
-        return (-b - sqrt(discriminant)) / (2.0*a);
+        return (-half_b - sqrt(discriminant)) / a;
     }
 }
 
@@ -51,6 +51,6 @@ int main(){
             png.saveColorSeq(pixel_color);
         }
     }
-    png.write("6.sphere_normalize.png");
+    png.write("6.sphereNormalize.png");
     return 0;
 }
