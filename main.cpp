c033cbd2de4a4e3ebcc98b06efe89fa5d7072d88
cosmos <kbxpy12321@gmail.com>
2020-08-04 15:14:12 -0500

    draw a ball

--- main.cpp
+++ main.cpp
@@ -2,7 +2,18 @@
 #include "types.h"
 #include "ray.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1, 0, 0);
     vec3 unit_direction = glm::normalize(r.direction());
     auto t = 0.5 * (unit_direction.y + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
@@ -33,6 +44,6 @@ int main(){
             png.saveColorSeq(pixel_color);
         }
     }
-    png.write("4.background.png");
+    png.write("5.hitsphere.png");
     return 0;
 }
