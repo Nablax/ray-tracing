eab6b808574933e01425d82e574281a58e69231c
cosmos <kbxpy12321@gmail.com>
2020-08-09 01:50:36 -0500

    one saved scene

--- main.cpp
+++ main.cpp
@@ -26,42 +26,43 @@ hittable_list random_scene() {
     auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
     world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
-//    for (int a = -11; a < 11; a++) {
-//        for (int b = -11; b < 11; b++) {
-//            auto choose_mat = random_double();
-//            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
-//
-//            if (glm::distance(center, point3(4, 0.2, 0)) > 0.9) {
-//                shared_ptr<material> sphere_material;
-//
-//                if (choose_mat < 0.8) {
-//                    // diffuse
-//                    auto albedo = random_vec3() * random_vec3();
-//                    sphere_material = make_shared<lambertian>(albedo);
-//                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-//                } else if (choose_mat < 0.95) {
-//                    // metal
-//                    auto albedo = random_vec3(0.5, 1);
-//                    auto fuzz = random_double(0, 0.5);
-//                    sphere_material = make_shared<metal>(albedo, fuzz);
-//                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-//                } else {
-//                    // glass
-//                    sphere_material = make_shared<dielectric>(1.5);
-//                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-//                }
-//            }
-//        }
-//    }
+    for (int a = -5; a < 5; a++) {
+        for (int b = -5; b < 5; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if (glm::distance(center, point3(4, 0.2, 0)) > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = random_vec3() * random_vec3();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.9) {
+                    // metal
+                    auto albedo = random_vec3(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
 
     auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material1));
+    world.add(make_shared<sphere>(point3(4, 1, 0), -0.95, material1));
 
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    auto material2 = make_shared<lambertian>(color(1, 0, 0.4));
     world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
 
     auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material3));
 
     return world;
 }
@@ -70,10 +71,10 @@ int main(){
     // Image
 
     const auto aspect_ratio = 3.0 / 2.0;
-    const int image_width = 400;
+    const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 50;
-    const int max_depth = 5;
+    const int samples_per_pixel = 500;
+    const int max_depth = 50;
 
     // World
 
@@ -81,7 +82,7 @@ int main(){
 
     // Camera
 
-    point3 lookfrom(13,2,3);
+    point3 lookfrom(13,2,4);
     point3 lookat(0,0,0);
     vec3 vup(0,1,0);
     auto dist_to_focus = 10.0;
@@ -90,6 +91,8 @@ int main(){
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
     Png_Image png(image_width, image_height);
+
+#pragma omp parallel for default(none) shared(png, cam, world)
     for (int row = 0; row < png.height(); row++) {
         for (int col = 0; col < png.width(); col++) {
             color pixel_color(0, 0, 0);
